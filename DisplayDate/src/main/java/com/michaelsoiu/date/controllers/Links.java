e57974a96f2303e5783470c5ad5ee81fe74ff5ff
Michael Soiu <michael.soiu@gmail.com>
2021-03-28 11:38:38 -0700

    my Display Date assignment

---
+++ DisplayDate/src/main/java/com/michaelsoiu/date/controllers/Links.java
@@ -0,0 +1,28 @@
+package com.michaelsoiu.date.controllers;
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Links {
+	@RequestMapping("/")
+	public String theLinks()	{
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/{dt}")
+	public String DateTime(@PathVariable("dt") String dt, Model model) {
+		if(!dt.equals("date") && !dt.equals("time"))
+			return "redirect:/";
+		SimpleDateFormat dm = (dt.equals ("date"))
+			? new SimpleDateFormat("EEEE, 'the' dd 'of' MMMM, YYYY")
+			: new SimpleDateFormat("h:mm a");
+		model.addAttribute("dtFormat", dm.format(new java.util.Date()));
+		model.addAttribute("dt", dt);
+		return "datetime.jsp";
+	}
+}
---
+++ DisplayDate/src/test/java/com/michaelsoiu/date/DisplayDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelsoiu.date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
